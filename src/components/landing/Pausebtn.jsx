8c4ad381e8caabd2345f1ebd04f43927eb36eede
amanpunia-510 <puniaaman888@gmail.com>
2024-10-05 06:31:20 +0530

    kg

--- src/components/landing/Pausebtn.jsx
+++ src/components/landing/Pausebtn.jsx
@@ -1,14 +1,23 @@
-"use client";
+"use client"
 import { useState } from 'react';
 import Icons from '../icons/Icons';
 
 const Pausebtn = () => {
+    const [isplay, setIsplay] = useState(false);
+    const click = () => {
+        if (isplay) {
+            setIsplay(false)
+        }
+        else {
+            setIsplay(true)
+        }
+    }
     return (
         <div className='w-full min-h-screen bg-white flex justify-center items-center'>
             <div className='size-20 bg-white rounded-full flex justify-center items-center shadow 
-            shadow-black cursor-pointer' onClick={click}>
+            shadow-black cursor-pointer active:scale-90 transition-all' onClick={click}>
                 <div className='size-5 flex items-center'>
-                    <Icons icons={isPlay ? 'pauseSvg' : 'playSvg'} />
+                    <Icons icons={isplay ? 'pauseSvg' : 'playSvg'} />
                 </div>
             </div>
         </div>
